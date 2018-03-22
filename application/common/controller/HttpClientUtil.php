1b1632ac788abe3485c84fec1d898face14d6c04
yuanxx <824264439@qq.com>
2018-03-22 17:44:22 +0800

    封装 Http请求 Header也可以定制。预留功能 2018年3月22日17:43:44

--- application/common/controller/HttpClientUtil.php
+++ application/common/controller/HttpClientUtil.php
@@ -4,29 +4,6 @@ namespace app\common\utils;
 
 class HttpClientUtil
 {
-    //get请求
-    public function requestGet($host, $path,$headers=[])
-    {
-        $method = "GET";
-        $appcode = "你自己的AppCode";
-        $headers = array();
-        array_push($headers, "Authorization:APPCODE " . $appcode);
-        $querys = "";
-        $bodys = "";
-        $url = $host . $path;
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_FAILONERROR, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, true);
-        if (1 == strpos("$" . $host, "https://")) {
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        }
-        var_dump(curl_exec($curl));
-    }
 
 }

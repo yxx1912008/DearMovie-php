e34f9d5e32d4f86f3d11e4235d874eebd235d3b4
yuanxx <824264439@qq.com>
2018-03-22 17:48:03 +0800

    公共的配置文件 例如豆瓣电影的Api 2018年3月22日17:47:26

--- application/index/config.php
+++ application/index/config.php
@@ -3,12 +3,18 @@
 return [
 
     //豆瓣api的host地址
-    'douban_api_host' => 'https://api.douban.com/',
+    'douban_api_host' => 'https://api.douban.com',
 
     //正在上映
     'in_theaters_path' => '/v2/movie/in_theaters',
 
+    //即将上映的电影
+    'coming_soon' => '/v2/movie/coming_soon',
 
+    //排名前250名的电影
+    'top250' => '/v2/movie/top250',
 
+    //关键字搜索电影
+    'search' => '/v2/movie/search',
 
 ];

5400448c113ac03472d0b0b441dd57efd8ae73eb
yuanxx <824264439@qq.com>
2018-03-27 16:41:23 +0800

    增加Member模块 该模块用户 会员信息管理 2018年3月27日16:41:08

---
+++ application/member/config.php
@@ -0,0 +1,15 @@
+<?php
+//配置文件
+return [
+
+    //获取openID url
+    'GET_OPENID_BASE_URL' => 'https://api.weixin.qq.com/sns/jscode2session',
+    //appid
+    'APP_ID' => 'wx0e2c2edbd333214b',
+    //APP SECRET
+    'APP_SECRET' => '7a9329971652ad1434436e8a1d32a312',
+    //盐 用来生成用户ID
+    'salt' => 'wedwqdw_+^*&^$%#$@!#$!@$'
+
+
+];
---
+++ application/member/controller/Index.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\member\controller;
+
+use app\member\model\WxUser;
+
+class Index
+{
+    public function index()
+    {
+        return '接口测试成功';
+    }
+
+
+    /**
+     * 获取微信用户信息，并保存到数据库
+     * 返回数据库用户ID
+     * 请求微信后台接口，交换code换取用户openId
+     * @param $code
+     * @param $nickName
+     * @param $avatarUrl
+     * @return \think\response\Json
+     */
+    public function getUserOpenId($code, $nickName, $avatarUrl)
+    {
+        $wxUser = new WxUser();
+        $host = config('GET_OPENID_BASE_URL');
+        $appId = config('APP_ID');
+        $secret = config('APP_SECRET');
+        $path = '?appid=' . $appId . '&secret=' . $secret . '&js_code=' . $code . '&grant_type=authorization_code';
+        $result = requestGetNoParam($host, $path, 1);
+        if (empty($result->errcode)) {
+            $openId = $result->openid;
+            $query = WxUser::getByOpenId($openId);
+            if (empty($query)) {
+                $wxUser['id'] = $this->getUserId();
+                $wxUser['open_id'] = $openId;
+                $wxUser['nick_name'] = $nickName;
+                $wxUser['avatar_url'] = $avatarUrl;
+                $wxUser->save();
+                $save_result = WxUser::getByOpenId($openId);
+                if (!empty($save_result)) {
+                    $returnData = ['result' => '1', 'id' => $save_result['id'], ['msg' => '用户注册成功']];
+                    return json($returnData);
+                }
+                $returnData = ['result' => '0', 'msg' => '用户注册失败！'];
+                return json($returnData);
+            }
+            $returnData = ['result' => '1', 'id' => $query['id'], 'msg' => '用户查询成功'];
+            return json($returnData);
+        }
+        $returnData = ['result' => '0', '' => '获取用户信息失败'];
+        return json($returnData);
+    }
+
+    /**
+     * 根据时间戳加盐
+     * 生成用户Id
+     * @return string
+     */
+    public function getUserId()
+    {
+        $salt = config('salt');
+        $time = time();
+        $saltWord = $salt . $time;
+        $result = md5($saltWord);
+        return $result;
+    }
+
+    public function testTime()
+    {
+
+        echo time() . '<br>';
+        $time = time();
+        echo 'time:' . strtotime($time);
+
+    }
+
+
+}

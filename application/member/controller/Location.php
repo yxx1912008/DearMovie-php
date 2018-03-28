e019a510f1f10a9cf4b9cb00f040b108bf2a38be
yuanxx <824264439@qq.com>
2018-03-28 11:51:01 +0800

    方便豆瓣Api识别，将'市'字去掉 2018年3月28日11:50:33

--- application/member/controller/Location.php
+++ application/member/controller/Location.php
@@ -25,15 +25,19 @@ class Location
      * @param string $location
      * @return \think\response\Json
      */
-    public function getUserCity($output = 'json', $pois = 1, $location = '30.28,120.15')
+    public function getUserCity($output = 'json', $pois = 0, $location = '30.28,120.15')
     {
+        //拼接请求
         $host = config('BAIDU_MAP_GET_LOCATION_API');
         $ak = config('BAIDU_MAP_AK');
         $path = 'location=' . $location . '&output=' . $output . '&pois=' . $pois . '&ak=' . $ak;
+        //设置请求参数 returnGetMsg =1 不在浏览器显示请求代码
         $result = requestGetNoParam($host, $path, 1);
         $status = $result->status;
         if ($status === 0) {
-            $returnData = ['result' => '1', 'city' => $result->result->addressComponent->city, 'msg' => '用户城市信息获取成功'];
+            $city = $result->result->addressComponent->city;
+            $city = str_replace('市', '', $city);
+            $returnData = ['result' => '1', 'city' => $city, 'msg' => '用户城市信息获取成功'];
             return json($returnData);
         }
         $returnData = ['result' => '0', 'msg' => '查询城市失败'];

1f83b46b6010bba0ba8b0219418e087d875bbbac
yuanxx <824264439@qq.com>
2018-04-03 16:07:35 +0800

    修复生成Token时出现的Bug 2018年4月3日16:07:12

--- application/member/common.php
+++ application/member/common.php
@@ -8,7 +8,7 @@
 function getToken()
 {
     $str = md5(uniqid(md5(microtime(true)), true));  //生成一个不会重复的字符串
-    $str = sha1($str + 'cs&%#$$%');  //加密
+    $str = sha1($str . 'cs&%#$$%');  //加密
     return $str;
 }
 
